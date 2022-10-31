72acd38c0c04a3239c03072b5d04584d398c5731
Emil-25 <emilrahimov25@gmail.com>
2022-10-31 22:35:45 +0400

    changed src of some images

--- src/app/main-hero/main-hero.component.ts
+++ src/app/main-hero/main-hero.component.ts
@@ -9,10 +9,10 @@ import 'tw-elements';
 export class MainHeroComponent implements OnInit {
 
   images = [
-    "../../assets/images/hero2.png",
-    "../../assets/images/hero3.png",
-    "../../assets/images/hero4.png",
-    "../../assets/images/hero5.png",
+    "./assets/images/hero2.png",
+    "./assets/images/hero3.png",
+    "./assets/images/hero4.png",
+    "./assets/images/hero5.png",
   ]
 
   constructor() { }
